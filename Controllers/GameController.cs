c7baa9719866ffee8535163fd1007da8d2184bcc
Dave McKinstry <dmckinstry@github.com>
2021-01-30 16:41:41 -0700

    Workaround for POST params

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -19,16 +19,23 @@ namespace GameServer.Controllers
             _logger = logger;
         }
 
-        // [HttpGet]
-        // public IEnumerable<Game> GetAllGames()
-        // {
-        //     return Game.ActiveGames;
-        // }
-
         [HttpGet]
+        public IEnumerable<Game> GetAllGames()
+        {
+            return Game.ActiveGames;
+        }
+
+        // [HttpGet]
         [HttpPost]
         public IEnumerable<Game> CreateNewGame(String player, int duration)
         {
+            // For some reason ASP.NET isn't correctly capturing params in the FORM body, so a temp workaround...
+            if ((player == null) && (this.Request.HasFormContentType) && (this.Request.Form["player"].Count > 0))
+            {
+                player = this.Request.Form["player"][0];
+                duration = int.Parse(this.Request.Form["duration"][0]);
+            }
+
             if (player != null)
                 new Game(player, duration );
             return Game.ActiveGames;
